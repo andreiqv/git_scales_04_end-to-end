53786509942b2bf5fa14c385987973c090eff2e2
z <kukharev.av@gmail.com>
2018-10-21 09:54:12 +0300

    1

--- training/run.py
+++ training/run.py
@@ -1,6 +1,6 @@
 import os
 os.system('python3 train_full_model.py -mb')
-os.system('python3 train_last_layer.py')
+os.system('python3 train_last_layer.py > _log_train.txt')
 os.system('python3 train_full_model.py -llr')
 
 
